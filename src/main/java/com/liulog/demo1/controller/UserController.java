99b68830ad7330071730b9c33dda66e847eee1cd
liam <liuyichaochina@gmail.com>
2023-05-20 22:28:55 +0800

    bili

--- src/main/java/com/liulog/demo1/controller/UserController.java
+++ src/main/java/com/liulog/demo1/controller/UserController.java
@@ -1,4 +1,10 @@
 package com.liulog.demo1.controller;
 
+import jdk.jfr.Frequency;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 public class UserController {
+
+    @RequestMapping(value = "/addUser",method = RequestMethod.POST)
 }
